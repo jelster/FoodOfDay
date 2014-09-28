bc23799485cec657ed7c8d184eba9d4bb75a0256
Josh Elster <josh@liquidelectron.com>
2014-09-28 13:43:21 -0500

    case sensitive input test integrated

--- FoodOfDay/Program.cs
+++ FoodOfDay/Program.cs
@@ -6,6 +6,33 @@ using System.Threading.Tasks;
 
 namespace FoodOfDay
 {
+    public enum MealTime
+    {
+        Indeterminate = 0,
+        Morning = 1,
+        Night = 2
+    }
+    public class Meal
+    {
+        public MealTime TimeOfDay { get; protected set; }
+
+        public Meal(MealTime mealTime)
+        {
+            TimeOfDay = mealTime;
+        }
+        protected Meal(string timeOfDay)
+        {
+            MealTime parsed;
+            Enum.TryParse<MealTime>(timeOfDay, true, out parsed);
+            TimeOfDay = parsed;            
+        }
+
+        public static Meal Create(string timeOfDay)
+        {
+            return new Meal(timeOfDay);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
--- FoodOfDayTests/TimeOfDayTests.cs
+++ FoodOfDayTests/TimeOfDayTests.cs
@@ -1,14 +1,41 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using FoodOfDay;
 
 namespace FoodOfDayTests
 {
-    
+
     public class TimeOfDayTests
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        [TestFixture]
+        public class given_a_morning
         {
+            const MealTime mealTime = MealTime.Morning;
+            
+
+            [TestFixture]
+            public class when_timeofday_is_specified
+            {
+                Meal sut;
+                const string dayTimeString = "morning";
+
+                [Test]
+                public void then_input_should_not_be_case_sensitive()
+                {
+                    sut = Meal.Create(dayTimeString);
+                    var enumSut = new Meal(MealTime.Night);
+                    var actual = sut.TimeOfDay;
+
+                    Assert.AreEqual(mealTime, actual);
+                }
+
+                [Test]
+                public void aaa()
+                {
+
+                }
+            }
         }
     }
 }
