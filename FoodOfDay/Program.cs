793a7856657674d7578c55067813e14e14e4089c
Josh Elster <josh@liquidelectron.com>
2014-09-28 14:02:46 -0500

    added dish types, values. Renamed and simplified tests

--- FoodOfDay/Program.cs
+++ FoodOfDay/Program.cs
@@ -12,9 +12,29 @@ namespace FoodOfDay
         Morning = 1,
         Night = 2
     }
+
+    public class DishType
+    {
+        public string Name { get; protected set; }
+        public IEnumerable<MealTime> MealsAllowed { get; protected set; }
+
+        public static readonly DishType Eggs = new DishType("Eggs", new[] { MealTime.Morning });
+        public static readonly DishType Steak = new DishType("Steak", new[] { MealTime.Night });
+        public static readonly DishType Toast = new DishType("Toast", new[] { MealTime.Morning });
+        public static readonly DishType Coffee = new DishType("Coffee", new[] { MealTime.Morning });
+        public static readonly DishType Wine = new DishType("Wine", new[] { MealTime.Night });
+        public static readonly DishType Cake = new DishType("Cake", new[] { MealTime.Night });
+        
+    }
     public class Meal
     {
         public MealTime TimeOfDay { get; protected set; }
+
+        public DishType Entree { get; protected set; }
+        public DishType Side { get; protected set; }
+        public DishType Drink { get; protected set; }
+        public DishType Dessert { get; protected set; }
+
 
         public Meal(MealTime mealTime)
         {
--- FoodOfDayTests/TimeOfDayTests.cs
+++ FoodOfDayTests/TimeOfDayTests.cs
@@ -7,34 +7,37 @@ namespace FoodOfDayTests
 
     public class TimeOfDayTests
     {
+        [TestFixture]
+        public class given_a_time_of_day
+        {
+            const MealTime mealTime = MealTime.Morning;
+            const string MorningString = "morning";
 
+            [TestFixture]
+            public class when_a_meal_is_created
+            {
+                Meal sut = Meal.Create(MorningString);
+
+                [Test]
+                public void then_the_input_time_string_should_not_be_case_sensitive()
+                {
+                    var actual = mealTime;
+                    Assert.AreEqual(sut.TimeOfDay, actual, "Input string was not parsed correctly");
+                }
+            }
+        }
+    }
+
+    public class DishTypeTests
+    {
         [TestFixture]
         public class given_a_morning
         {
-            const MealTime mealTime = MealTime.Morning;
-            
-
-            [TestFixture]
-            public class when_timeofday_is_specified
+            Meal sut;
+            [Test]
+            public void aa()
             {
-                Meal sut;
-                const string dayTimeString = "morning";
-
-                [Test]
-                public void then_input_should_not_be_case_sensitive()
-                {
-                    sut = Meal.Create(dayTimeString);
-                    var enumSut = new Meal(MealTime.Night);
-                    var actual = sut.TimeOfDay;
-
-                    Assert.AreEqual(mealTime, actual);
-                }
-
-                [Test]
-                public void aaa()
-                {
-
-                }
+                Assert.AreEqual(sut.Entree, DishType.Eggs);
             }
         }
     }
