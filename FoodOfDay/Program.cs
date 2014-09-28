cea870ec5b945191b42f3797eed8422d681db0fc
Josh Elster <josh@liquidelectron.com>
2014-09-28 14:50:22 -0500

    defined dishes, refactored factory/ctor

--- FoodOfDay/Program.cs
+++ FoodOfDay/Program.cs
@@ -13,43 +13,95 @@ namespace FoodOfDay
         Night = 2
     }
 
-    public class DishType
+    public enum DishType {
+        Indeterminate = 0,
+        Entree = 1,
+        Side = 2,
+        Drink = 3,
+        Dessert = 4
+    }
+
+    public class Dish
     {
+
+        public static readonly Dish Eggs = new Dish("Eggs", DishType.Entree, new[] { MealTime.Morning });
+        public static readonly Dish Steak = new Dish("Steak", DishType.Entree, new[] { MealTime.Night });
+        public static readonly Dish Toast = new Dish("Toast", DishType.Side, new[] { MealTime.Morning });
+        public static readonly Dish Potato = new Dish("Potato", DishType.Side, new[] { MealTime.Night });
+
+        public static readonly Dish Coffee = new Dish("Coffee", DishType.Drink, new[] { MealTime.Morning });
+        public static readonly Dish Wine = new Dish("Wine", DishType.Drink, new[] { MealTime.Night });
+        public static readonly Dish Cake = new Dish("Cake", DishType.Dessert, new[] { MealTime.Night });
+
+        public static IEnumerable<Dish> Entrees { get { return new[] { Dish.Eggs, Dish.Steak }; } }
+        public static IEnumerable<Dish> Sides { get { return new[] { Dish.Toast, Dish.Potato }; } }
+        public static IEnumerable<Dish> Drinks { get { return new[] { Dish.Wine, Dish.Coffee }; } }
+        public static IEnumerable<Dish> Desserts { get { return new[] { Dish.Cake }; } }
+        
         public string Name { get; protected set; }
         public IEnumerable<MealTime> MealsAllowed { get; protected set; }
 
-        public static readonly DishType Eggs = new DishType("Eggs", new[] { MealTime.Morning });
-        public static readonly DishType Steak = new DishType("Steak", new[] { MealTime.Night });
-        public static readonly DishType Toast = new DishType("Toast", new[] { MealTime.Morning });
-        public static readonly DishType Coffee = new DishType("Coffee", new[] { MealTime.Morning });
-        public static readonly DishType Wine = new DishType("Wine", new[] { MealTime.Night });
-        public static readonly DishType Cake = new DishType("Cake", new[] { MealTime.Night });
-        
+        public DishType Kind { get; set; }
+
+        protected Dish(string name, DishType dish, IEnumerable<MealTime> times)
+        {
+            Name = name;
+            Kind = dish;
+            MealsAllowed = times ?? Enumerable.Empty<MealTime>();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}", Name, Kind);
+        }
+
     }
     public class Meal
     {
         public MealTime TimeOfDay { get; protected set; }
 
-        public DishType Entree { get; protected set; }
-        public DishType Side { get; protected set; }
-        public DishType Drink { get; protected set; }
-        public DishType Dessert { get; protected set; }
+        public Dish Entree { get; protected set; }
+        public Dish Side { get; protected set; }
+        public Dish Drink { get; protected set; }
+        public Dish Dessert { get; protected set; }
 
 
-        public Meal(MealTime mealTime)
+        protected Meal(MealTime mealTime)
         {
             TimeOfDay = mealTime;
+            if (TimeOfDay == MealTime.Morning)
+            {
+                Entree = Dish.Entrees.FirstOrDefault(x => x.MealsAllowed.Contains(MealTime.Morning));
+                Side = Dish.Sides.FirstOrDefault(x => x.MealsAllowed.Contains(MealTime.Morning));
+                Drink = Dish.Drinks.FirstOrDefault(x => x.MealsAllowed.Contains(MealTime.Morning));
+            }
+            else if (TimeOfDay == MealTime.Night)
+            {
+                Entree = Dish.Entrees.FirstOrDefault(x => x.MealsAllowed.Contains(MealTime.Night));
+                Side = Dish.Sides.FirstOrDefault(x => x.MealsAllowed.Contains(MealTime.Night));
+                Drink = Dish.Drinks.FirstOrDefault(x => x.MealsAllowed.Contains(MealTime.Night));
+                Dessert = Dish.Desserts.FirstOrDefault(x => x.MealsAllowed.Contains(MealTime.Night));
+            
+            }
+            else
+            {
+                throw new ArgumentException("Specified MealTime is invalid.");
+            }
+
         }
-        protected Meal(string timeOfDay)
-        {
-            MealTime parsed;
-            Enum.TryParse<MealTime>(timeOfDay, true, out parsed);
-            TimeOfDay = parsed;            
-        }
+      
 
         public static Meal Create(string timeOfDay)
         {
-            return new Meal(timeOfDay);
+            MealTime parsed;
+            Enum.TryParse<MealTime>(timeOfDay, true, out parsed);
+                      
+            return new Meal(parsed);
+        }
+
+        public static Meal Create(MealTime timeofDay)
+        {
+            return new Meal(timeofDay);
         }
 
     }
