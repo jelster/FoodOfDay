4c8a632b7590c38b134309a3926eae30ffff526b
Josh Elster <josh@liquidelectron.com>
2014-09-29 00:28:01 -0500

    added console integration test class, command line parsing tests

--- FoodOfDay/Program.cs
+++ FoodOfDay/Program.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace FoodOfDay
 {
-   
-    
-    class Program
+    public class FoodConsoleOptions
     {
+        public MealTime TimeOfDay { get; set; }
+        public IEnumerable<DishType> FoodOrder { get; set; }
+
+        public static FoodConsoleOptions Parse(string[] p)
+        {
+            MealTime time;
+            Enum.TryParse(p[0], true, out time);
+            return new FoodConsoleOptions() { TimeOfDay = time };
+        }
+    }
+    public class FoodConsole
+    {
+
+
         static void Main(string[] args)
         {
+
+
+            var options = FoodConsoleOptions.Parse(args);
+
         }
     }
 }
---
+++ FoodOfDayTests/ConsoleTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using FoodOfDay;
+
+namespace FoodOfDayTests
+{
+    [TestFixture]
+    public class ConsoleTests
+    {
+        [Test]
+        public void CommandLineShouldParseTimeOfDay()
+        {
+            var opts = FoodConsoleOptions.Parse(new[] { "morning" });
+            Assert.AreEqual(MealTime.Morning, opts.TimeOfDay); 
+        }
+    }
+}
